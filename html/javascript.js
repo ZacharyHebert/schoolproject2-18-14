bfb97e369d13a99ef3c750b36ce09dbface252b5
stevenwashere <rodriguezsteven06@gmail.com>
2014-03-03 10:53:45 -0800

    random number gen
    Powell wanted the website to give a random number for the student to
    work on.

--- html/javascript.js
+++ html/javascript.js
@@ -3,6 +3,7 @@ File: 		javascript.js
 Purpose:	Contains all external javascript for project. If multiple javascript files are created, edit this purpose.
 */
 
+var randNum=Math.floor((Math.random()*255)+1);
 var calculate(decimal){
 	
 };
