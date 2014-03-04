c48f5e0cb11e96fa4745990b3ca2f578982f821f
stevenwashere <rodriguezsteven06@gmail.com>
2014-03-04 14:51:16 -0800

    Decimal To Binary
    Found this code on javascriptkit.com

--- html/javascript.js
+++ html/javascript.js
@@ -7,3 +7,11 @@ var randNum=Math.floor((Math.random()*255)+1);
 var calculate(decimal){
 	
 };
+function dec2bin() { // found on javascriptkit.com
+var x = document.getElementById("deci").value;
+if ((/[^0-9]/g.test(x)) || x == "") {
+alert ("You must enter an integer decimal number!");
+document.getElementById("deci").value = "";
+document.getElementById("deci").focus();
+return false;
+}
